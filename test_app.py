3878d070e101d9d0009dd84f8f8e82bb344ae9aa
senior_sacheen <defsacheen@gmail.com>
2026-07-12 21:11:06 +0530

    test change

--- test_app.py
+++ test_app.py
@@ -2,9 +2,11 @@ from app import app
 
 client = app.test_client()
 
+
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
+
 
 def test_health():
     response = client.get("/health")
